b6eec4f67ae1fd240b175562656632a4c73bc1e8
Tanuj Vishnoi <tanujkumarvishnoi@qainfotech.com>
2018-09-06 18:33:36 +0530

    SpringBoot Todays Practise

---
+++ src/main/java/WebService/SpringBootExamples/App.java
@@ -0,0 +1,13 @@
+package WebService.SpringBootExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
